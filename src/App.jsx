8c5158e166a2dcdae76ae7e2021c08e868e1e93b
cosmincu <hello@cosminursea.eu>
2026-06-30 13:38:06 +0200

    Reorganize project structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CompleteSharedLifeDashboard from './CompleteSharedLifeDashboard';
+import './apple-design.css';
+import './index.css';
+
+function App() {
+  return <CompleteSharedLifeDashboard />;
+}
+
+export default App;
